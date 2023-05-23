aa94df003e16a39f48af9eef759e70df19c24c7c
barryinvisible <31002774+barryinvisible@users.noreply.github.com>
2023-05-23 13:07:19 +0300

    new

---
+++ script.js
@@ -0,0 +1,12 @@
+// var tab = document.querySelector('.tab-icon'),
+//     content = document.querySelector('.tab-content');
+
+// tab.addEventListener('click', function hideTabsContent() {
+//     if (tab.classList.contains('border') && content.classList.contains('border')) {
+//         tab.classList.add('white-border');
+//         content.classList.remove('hide');
+//         content.classList.add('show');
+//     }
+// });
+
+// for (var i = a; i < content)
